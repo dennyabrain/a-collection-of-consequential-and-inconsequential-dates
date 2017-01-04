37dd9d3d362fa6c442e6a690da103ddc7a11451f
dennyabrain <denny.george90@gmail.com>
2017-01-04 11:37:45 -0500

    first commit after cloning from the postcard project

---
+++ js/item.js
@@ -0,0 +1,66 @@
+/**
+ * Created by abrain on 1/3/17.
+ */
+
+var item = [
+    {
+        date:'December 26, 2016',
+        post:'I feel slightly bothered by the fact that you did not wish me on Christmas.'
+    },
+    {
+        date:'December 29, 2016',
+        post:'I remember that you were never really a Christmas-Wisher, anyway. Maybe I should not be that upset about it.'
+    },
+    {
+        date:'January 1, 2017',
+        post:'I realize that I forgot to wish you on your birthday and thereby loose all rights to be upset about you not wishing me for Christmas. Its not like the birth of Our Lord Saviour Jesus Christ is more important that your birthday.'
+    },
+    {
+        date:'December 17, 2016',
+        post:'I go to an art exhibition on digital surveillance and see some 3D-like postcards. They look like something that a strange being like you may cherish. There is a chance that you would just go \'meh\' and toss them into a pile of nothings. There is also the possibility that you would totally go berserk about how cool they are and send it to loved ones. Since that is a rare occasion I am willing to tell the convinience-seeking side of my brain to lay low this time. I start thinking about the right occasion to post these to you'
+    },
+    {
+        date:'July 2, 2013',
+        post:'You tell me you like postcards. I might be completely making up this one or partially misremembering something else 😜'
+    },
+    {
+        date:'August 17, 2015',
+        post:'You post a letter from Berkley to me hence revealing your address.'
+    },
+    {
+        date:'November 10, 2014',
+        post:'I think for the first time after being in the US that maybe I should send you a gift, but the logistics of international postage seemed like the valid excuse (back then) for me to not send you anything.'
+    },
+    {
+        date:'October 9, 2015',
+        post:'I decided I should definitely send you a present but what could I send that is not generic enough to feel meaningless and also not meaningful enough to feel like a subtle sublimal message 😜'
+    },
+    {
+        date:'December 27, 2016',
+        post:'I watch a really good movie La La Land and think of you. I wanted to recommend you to watch it but am too taken over by my ego because of the fact that you did not wish me on Christmas 😜'
+    },
+    {
+        date:'December 6, 2016',
+        post:'You post timelapse images of locations on your blog that makes a light go off in my head. Even though you are a nerd of many things, since I do not share a lot of those passions with you, it is hard to place a finger on what physical object you might like. I think, is it possible that a LOTR reading, Ursula Le Guin loving, google timelapse publishing being might be into Maps? 😲'
+    },
+    {
+        date:'December 17, 2016',
+        post:'I am mildly dissapointed that these postcards are free. Because they feel like they should cost money. They look like something of value must be exchanged in order to acquire them. I prepare myself to to spend around 20 dollars for two postcard. I feel that if you like them, it would be nice for you to keep one for yourself and send the other one to a loved one. Since they were free I picked up all unique copies and also some not so good postcards. I am sure you would want to send postcards to your not-that-much-loved-ones too 😜'
+    },
+    {
+        date:'January 2, 2017',
+        post:'I go to USPS to post these. I put them in an envelope because I am too worried that if I post them actually as postcards they will get stolen coz they look so good. I had to cancel plans because the post office was closed due to them observing an extended new year\'s eve.'
+    },
+    {
+        date:'January 2, 2017',
+        post:'This was not planned at all but you happen to ping me and we talk on phone after so long. Throughout the call I am worried that your birthday will come up. When it does, I literally grab my neck to stop me from saying that \'Actually I was planning a surprise for you\'. I believe that it remains the best display of my growth and character in the past 5 years 😜'
+    },
+    {
+        date:'January 3, 2017',
+        post:'I double check your address with Ruchita and Sagar after I post the package. So denny of me 😜.'
+    },
+    {
+        date:'January 1, 2017',
+        post:'I finally figured out what the letter accompanying the postcards should be like.'
+    }
+];
